e7017cb053ef6c353ec97ed6bcf7d8c9749e96c2
adhanadeepak <deepakadhana93@gmail.com>
2019-07-30 19:20:24 +0530

    Title component updated

--- src/elements/Heading.js
+++ src/elements/Heading.js
@@ -10,28 +10,31 @@ const Title = (props) =>{
 const Explore = styled.div`
     ${props => props.borderRight ? 'border-right' : 'border-left'}: 3px solid #ffc300;
     color: #12443d;
+    text-align: ${props => props.textAlign || 'left'};
      ${props => props.borderRight ? 'padding-right' : 'padding-left'}: 8px;
-`
+     margin-bottom: 10px;
+`;
 
 const HeadingWrapper = styled.div`
-    
-`
+    line-height: 1;
+    text-align: ${props => props.textAlign || 'left'};
+`;
 const HeadingLight = styled.span`
      display: ${props => props.display };
-`
+`;
 const HeadingBold = styled.span`
      display: ${props => props.display };
-`
+`;
 
     return(
         <Wrapper className={props.wrapperClass} {...props}>
-            <Explore borderRight={props.borderRight}>Explore</Explore>
-            <HeadingWrapper className="text-sukoon inline text-4xl w-full">
-                <HeadingLight className="font-light mr-2" display={props.display}>{props.titleLight || ''}</HeadingLight>
-                <HeadingBold className={`font-semibold ${props.addClass}`} display={props.display}>{props.titleBold || ''}</HeadingBold>
+            <Explore textAlign={props.textAlign} borderRight={props.borderRight}>{props.mainHeading || 'Explore'}</Explore>
+            <HeadingWrapper textAlign={props.textAlign} className="text-sukoon inline text-4xl w-full">
+                <HeadingLight className="font-light mt-3 mr-2" display={props.display}>{props.titleLight || ''}</HeadingLight>
+                <HeadingBold className={`font-semibold mb-3 ${props.addClass}`} display={props.display}>{props.titleBold || ''}</HeadingBold>
             </HeadingWrapper>
         </Wrapper>
     )
-}
+};
 
 export default Title;
