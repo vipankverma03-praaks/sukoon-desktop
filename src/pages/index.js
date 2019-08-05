b8f04da101cb750296cc1cbd3c3a237fe25ca21a
Pixelronnie <46241394+Pixelronnie@users.noreply.github.com>
2019-08-05 18:05:35 +0530

    index-page-banner-comp-pick

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,82 +19,7 @@ import BlogImg  from "../images/iStock-968194620_2019-07-17/iStock-968194620@3x.
 import HeaderImg from "../images/Desktop-Header/DesktopHeader.jpg"
 import Slider from "react-slick";
 import Physician from "../images/physician_retail_web_2019-07-29/physician_retail_web@3x.jpg";
-
-// Intro Section
-const Heading = () => {
-    return (
-        <div className="text-sukoon text-v6xl">
-            <h2 className="font-light block">
-                Don't Just Stay,
-            </h2>
-            <span className="font-extrabold block">
-                Live Here !
-            </span>
-        </div>
-    )
-};
-
-const HeaderIntroLeft = styled.div`
-        top:  10vw;
-        left: -18vw;
-       
-       @media screen and (max-width: 1600px){
-       left: -16vw;
-       }
-       @media screen and (max-width: 1400px){
-       left: -14vw;
-       }
-       @media screen and (max-width: 1200px){
-       left: -10vw;
-       }
-       
-   
-    `;
-const HeaderIntroRight = styled.div`
-    
-    `;
-
-const HeaderIntro = () => {
-
-    return (
-        <section id="header-intro" className="w-full">
-            <div id="header-intro-container" className="flex relative">
-                <div className="w-2/5">
-                </div>
-                <HeaderIntroRight id="header-intro-right" className="relative">
-                    <HeaderIntroLeft id="header-intro-left" className="absolute w-fit">
-                        <Heading/>
-                        <div id="contact-info-container" className="p-4 bg-sukoon w-full flex">
-                            <select name="appointment" id="appointment-type"
-                                    className="block p-2 bg-sukoon text-white outline-none w-1/2 appointment-type mx-2 font-medium"
-                                    value="">
-                                <option value="" selected>Appointment Type</option>
-                                <option value="">In House</option>
-                                <option value="">Online Consulting</option>
-                            </select>
-                            <input type="text" placeholder="Email / Phone"
-                                   className="block bg-transparent outline-none customer-contact-info w-1/2 p-2 text-sukoon ml-4 text-white"/>
-                        </div>
-                        <div className="book-now-container mt-4">
-                            <div className="book-now-inner-container p-1">
-                                <button
-                                    className="block relative outline-none  items-center py-2 px-4 bg-sukoon text-white">
-                                    <span className="text-sm font-normal">Book Now</span>
-                                </button>
-                            </div>
-                            <div className="my-4">
-                                <span>Or Call Us  +91 9876543210</span>
-                            </div>
-                        </div>
-                        <Link to="/" className="underline">Discover more!</Link>
-                    </HeaderIntroLeft>
-                    <img src={HeaderImg} className="block" alt="Header"/>
-                </HeaderIntroRight>
-            </div>
-        </section>
-    )
-};
-
+import Banner from "../components/Banner";
 
 const WhySukoon = () => {
     return (
@@ -673,7 +598,7 @@ function IndexPage(props) {
                 title="Home"
                 keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
             />
-            <HeaderIntro/>
+            <Banner/>
             <WhySukoon/>
             <InPatientServices/>
             <OutPatientServices/>
