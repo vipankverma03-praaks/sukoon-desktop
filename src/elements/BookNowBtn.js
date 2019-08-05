113f4e4114e7f9f78edb90e56a781f9c47ec46dd
adhanadeepak <deepakadhana93@gmail.com>
2019-08-05 18:51:28 +0530

    Payment button for booking page.

--- src/elements/BookNowBtn.js
+++ src/elements/BookNowBtn.js
@@ -3,16 +3,16 @@ import styled from "styled-components";
 
 
 const Wrapper = styled.div`
-  border: 1px solid #12443e;
-  padding: ${props => props.border ? '2px' : ''};
-  background: ${props => props.border ? 'white' : '#12443e'};
+  border: ${props => props.type === 'white' ? '1px solid white': '1px solid #12443e'};
+  padding: ${props => props.border ? '2px' : props.type === 'white' ? '5px' : ''};
+  background: ${props => props.border ? 'white' : (props.type === 'white' ? 'transparent' : '#12443e')};
 `;
 const Button = (props) =>{
 
   return(
-    <Wrapper border={props.border} className="w-fit">
+    <Wrapper border={props.border}  type={props.type} className={`${props.type === 'white' ? 'float-right' : ''} w-fit`}>
       <button
-        className="block relative outline-none text-sm items-center py-2 px-4 bg-sukoon text-white">
+        className={`${props.type === 'white' ? 'bg-white text-sukoon font-bold': 'bg-sukoon'} block relative font-gilroyMedium outline-none text-sm items-center py-2 px-4  text-white`}>
         {props.children}
       </button>
     </Wrapper>
